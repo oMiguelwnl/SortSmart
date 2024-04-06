afc78e2deda2943b4b4ee680b1150ec0c5fc08be
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-04-06 12:23:52 -0300

    feat: add all products to the Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,14 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Product } from "@/db";
+import type { Product as TProduct } from "@/db";
 import { cn } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import { QueryResult } from "@upstash/vector";
 import { ChevronDown, Filter, SortAsc } from "lucide-react";
 import { useState } from "react";
 import axios from "axios";
+import Product from "@/components/Products/Product";
 
 const SORT_OPTIONS = [
   { name: "None", value: "none" },
@@ -27,7 +28,7 @@ export default function Home() {
   const { data: products } = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
-      const { data } = await axios.post<QueryResult<Product>[]>(
+      const { data } = await axios.post<QueryResult<TProduct>[]>(
         "http://localhost:3000/api/products",
         {
           filter: {
@@ -78,6 +79,19 @@ export default function Home() {
           </button>
         </div>
       </div>
+      <section className="pb-24 pt-6">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+          {/* Filters */}
+          <div></div>
+
+          {/* Product grid */}
+          <ul className="lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+            {products?.map((product) => (
+              <Product product={product.metadata!} key={product.id} />
+            ))}
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
