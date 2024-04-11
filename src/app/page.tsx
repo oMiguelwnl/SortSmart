9c6a8e6a75c897e6cd9648396fc59036a34d2476
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-04-11 11:49:55 -0300

    feat: add Size Filters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,6 +40,16 @@ const COLOR_FILTERS = {
   ] as const,
 };
 
+const SIZE_FILTERS = {
+  id: "size",
+  name: "Size",
+  options: [
+    { value: "S", label: "S" },
+    { value: "M", label: "M" },
+    { value: "L", label: "L" },
+  ],
+} as const;
+
 const SUBCATEGORIES = [
   { name: "T-Shirts", selected: true, href: "#" },
   { name: "Hoodies", selected: false, href: "#" },
@@ -56,7 +66,6 @@ export default function Home() {
     size: ["L", "M", "S"],
     sort: "none",
   });
-  console.log(filter);
 
   const { data: products } = useQuery({
     queryKey: ["products"],
@@ -175,6 +184,40 @@ export default function Home() {
                         />
                         <label
                           htmlFor={`color-${optionIdx}`}
+                          className="ml-3 text-sm text-gray-600"
+                        >
+                          {option.label}
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+
+              {/* Size Filters */}
+              <AccordionItem value="size">
+                <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
+                  <span className="font-medium text-gray-900">Size</span>
+                </AccordionTrigger>
+
+                <AccordionContent className="pt-6 animate-none">
+                  <ul className="space-y-4">
+                    {SIZE_FILTERS.options.map((option, optionIdx) => (
+                      <li key={option.value} className="flex items-center">
+                        <input
+                          type="checkbox"
+                          id={`size-${optionIdx}`}
+                          onChange={() => {
+                            applyArrayFilter({
+                              category: "size",
+                              value: option.value,
+                            });
+                          }}
+                          checked={filter.size.includes(option.value)}
+                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        />
+                        <label
+                          htmlFor={`size-${optionIdx}`}
                           className="ml-3 text-sm text-gray-600"
                         >
                           {option.label}
