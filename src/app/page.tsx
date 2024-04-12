0d30fc91751e56f3f37bf3c8bf2c374dafbfe1e2
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-04-12 10:42:42 -0300

    feat: add Custom Price Filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { ProductState } from "@/lib/validators/product-validator";
+import { Slider } from "@/components/ui/slider";
 
 const SORT_OPTIONS = [
   { name: "None", value: "none" },
@@ -114,6 +115,9 @@ export default function Home() {
       }));
     }
   };
+
+  const minPrice = Math.min(filter.price.range[0], filter.price.range[1]);
+  const maxPrice = Math.max(filter.price.range[0], filter.price.range[1]);
 
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -276,6 +280,70 @@ export default function Home() {
                         </label>
                       </li>
                     ))}
+                    <li className="flex justify-center flex-col gap-2">
+                      <div>
+                        <input
+                          type="radio"
+                          id={`price-${PRICE_FILTERS.options.length}`}
+                          onChange={() => {
+                            setFilter((prev) => ({
+                              ...prev,
+                              price: {
+                                isCustom: true,
+                                range: [0, 100],
+                              },
+                            }));
+                          }}
+                          checked={filter.price.isCustom}
+                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        />
+                        <label
+                          htmlFor={`price-${PRICE_FILTERS.options.length}`}
+                          className="ml-3 text-sm text-gray-600"
+                        >
+                          Custom
+                        </label>
+                      </div>
+                      <div className="flex justify-between">
+                        <p className="font-medium">Price</p>
+                        <div>
+                          {filter.price.isCustom
+                            ? minPrice.toFixed(0)
+                            : filter.price.range[0].toFixed(0)}{" "}
+                          $ -{" "}
+                          {filter.price.isCustom
+                            ? maxPrice.toFixed(0)
+                            : filter.price.range[1].toFixed(0)}{" "}
+                          $
+                        </div>
+                      </div>
+                      <Slider
+                        className={cn({ "opacity-50": !filter.price.isCustom })}
+                        disabled={!filter.price.isCustom}
+                        onValueChange={
+                          (range) => {
+                            const [newMin , newMax] =range
+
+                            setFilter((prev) => ({
+                              ...prev,
+                              price:{
+                                isCustom:true,
+                                range: [newMin, newMax]
+                              }
+                            }))
+                          }
+                        }
+                        value={
+                          filter.price.isCustom
+                            ? filter.price.range
+                            : DEFAULT_CUSTOM_PRICE
+                        }
+                        min={DEFAULT_CUSTOM_PRICE[0]}
+                        defaultValue={DEFAULT_CUSTOM_PRICE}
+                        max={DEFAULT_CUSTOM_PRICE[1]}
+                        step={5}
+                      />
+                    </li>
                   </ul>
                 </AccordionContent>
               </AccordionItem>
