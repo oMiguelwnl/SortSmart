a4601bd287780a1ffe4cfb635ff041da88a7d366
Miguel Rafael <contato.Miguel.almeida7@gmail.com>
2024-05-16 11:12:03 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -133,7 +133,7 @@ export default function Home() {
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24 ">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-          High-Quality cotton selection
+          SortSmart
         </h1>
 
         <div className="flex items-center">
